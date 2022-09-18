24650a7de677090733e2610a97106bc3296d421f
rktdnjs <rktdnjs@naver.com>
2022-09-18 18:31:17 +0900

    2-10 icons

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import * as React from 'react';
 import * as Location from 'expo-location';
+import * as Font from "expo-font";
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ScrollView, Dimensions, ActivityIndicator } from 'react-native';
 // import Constants from 'expo-constants';
 
 const {width : SCREEN_WIDTH} = Dimensions.get('window');
+
 const API_KEY = "91466d74673c81a3f639ea04fea0d36a"; //안전한 방법은 아님, 원래는 서버에...!
+
+//받은 데이터의 이름을 보내서 해당 아이콘의 이름에 맞게 변경해 줌
+//강의와 다르게, Ionicons를 썼는데 그냥 Fontisto쓰는걸 추천 ㅎㅎ
+const icons = {
+  Clouds : "cloudy",
+  Clear : "sunny-outline",
+  Atmosphere: "cloudy",
+  Snow: "snow",
+  Rain: "rainy",
+  Drizzle: "rainy",
+  Thunderstorm: "thunderstorm",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
-  const [ok, setOk] = useState(true);
+  const [ok, setOk] = useState(true); //별 다른 사용은 x
+
   const getWeather = async() => {
     const {granted} = await Location.requestForegroundPermissionsAsync();
     if(!granted) { //유저가 위치 정보 권한 요청을 거절
@@ -51,7 +67,13 @@ export default function App() {
         days.map((day, index) => 
         <View key={index} style={styles.day}>
           {/* 여기서 toFixed는 소수점 1자리 까지라는 뜻 */}
-          <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}</Text>
+          <View style={{flexDirection:"row", 
+          alignItems:"center", 
+          justifyContent:"space-between",
+          width:"100%"}}>
+            <Text style={styles.temp}>{parseFloat(day.temp.day).toFixed(1)}도</Text>
+            <Ionicons name={icons[day.weather[0].main]} size={70} color="white"/>
+          </View>
           <Text style={styles.description}>{day.weather[0].main}</Text>
         </View>
         ))}
@@ -63,31 +85,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'tomato',
+    backgroundColor: 'lightgreen',
   },
   city: {
     flex: 1.2,
-    backgroundColor: 'tomato',
+    backgroundColor: 'lightgreen',
     justifyContent: 'center',
     alignItems: 'center',
   },
   cityName: {
-    fontSize: 68,
+    fontSize: 70,
     fontWeight: '500',
-  },
-  weather: {
-    
+    color: "white",
   },
   day: {
     width : SCREEN_WIDTH, //사용하는 디바이스 너비 만큼의 공간을 차지하게 됨
-    alignItems: 'center',  
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
+    fontWeight: "600",
     marginTop: 50,
-    fontSize: 178,
+    fontSize: 80,
+    color:"white"
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    fontWeight: "500",
+    marginTop: -10,
+    fontSize: 30,
+    color: "white",
   },
 });
