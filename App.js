f475427f1cfe71fab2e1cede05e2f59a178f2f7c
rktdnjs <rktdnjs@naver.com>
2022-10-06 12:58:43 +0900

    14.save

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      <Text>Another Component</Text>
+      <Button title='New Button!'></Button>
     </View>
   );
 }
