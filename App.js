d39ab8e19db42f6f0631767485fa864a223ba272
rktdnjs <rktdnjs@naver.com>
2022-10-07 12:16:45 +0900

    22

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+
+  function goalInputHandler(Text) {
+    setInputText(Text);
+  };
+
+  function addGoalHandler() {
+    console.log(inputText);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='목표를 입력하세요!'/>
-        <Button title="목표 추가하기"/>
+        <TextInput 
+          style={styles.textInput} 
+          placeholder='목표를 입력하세요!'
+          onChangeText={goalInputHandler}
+        />
+        <Button title="목표 추가하기" onPress={addGoalHandler}/>
         {/* 버튼의 경우 style 속성을 적용할 수 없다. 그렇기 때문에 다른 방법으로 수정 
         inputContainer의 alignItems를 수정하여 버튼이 늘어나지 않도록 함 */}
       </View>
@@ -21,7 +36,8 @@ const styles = StyleSheet.create({
   appContainer: {
     flex:1, // 제일 바깥의 컨테이너가 1개 뿐이므로 외부 컨테이너가 전체 높이를 차지함
     // 이후 내부 컨테이너들이 비율을 1:3으로 나눠서 가짐!
-    paddingTop: 50
+    paddingTop: 50,
+    backgroundColor:'#7fffd4'
   },
   inputContainer: {
     flex: 1,
